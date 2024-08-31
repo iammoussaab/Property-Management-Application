a9c60736d0f0ef0d0097e50a060aae40036401e9
iammoussaab <iammoussaab@gmail.com>
2024-08-31 23:53:13 +0100

    Add new dependencies and configuration files

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const auth = require('./middleware/auth');
+const propertyRoutes = require('./routes/property');
+const tenantRoutes = require('./routes/tenant');
+const paymentRoutes = require('./routes/payment');
 
 dotenv.config();
 const app = express();
@@ -10,13 +14,12 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: false, useUnifiedTopology: false })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => {
 		console.log('Connected to MongoDB successfully!');
 		app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 	})
 	.catch((err) => console.log('Failed to connect to MongoDB:', err));
-
 
 app.use('/api/properties', auth, propertyRoutes);
 app.use('/api/tenants', auth, tenantRoutes);
